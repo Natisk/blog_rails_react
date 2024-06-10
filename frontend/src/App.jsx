7e1b3c52e12c77f213e19e0532e6918f4de5bf76
Vitaly Matseiko <realnatisk@gmail.com>
2024-06-10 22:23:56 +0200

    react set up and add postsList feature

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import PostsList from './features/posts/PostsList';
+
+function App() {
+  return (
+    <>
+      <h1>React on Rails</h1>
+      <PostsList/>
+    </>
+  )
+}
+
+export default App
